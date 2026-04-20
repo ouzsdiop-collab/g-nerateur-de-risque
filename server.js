4921a07f99e4b051a93f957779cd4350ab2bfbef
ouzsdiop-collab <ouzsdiopl@gmail.com>
2026-04-20 13:08:57 +0200

    bind server to 0.0.0.0 for railway

--- server.js
+++ server.js
@@ -1008,6 +1008,6 @@ function formatPlanAsText(s) {
   ].join("\n");
 }
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
